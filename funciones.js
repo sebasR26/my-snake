f807b4be29f374ac096a1165b57ee56bafd48ad8
sebasR26 <sebassrp47@gmail.com>
2024-02-07 17:39:54 -0500

    mejoras

--- funciones.js
+++ funciones.js
@@ -5,12 +5,31 @@ let foodX= 1, foodY = 1;
 //coordenadas de la cabeza
 let snakeX=8, snakeY= 22;
 
+//variable para limites del juego
+gameOver = false;
+
+//variable para el cuerpo
 let snakeBody = [];
 
 let velocityX = 0, velocityY = 0;
 
 const playBoard = document.querySelector(".play-board")
+const scoreELement =  document.querySelector(".score")
+const highScoreELement =  document.querySelector(".high-score")
 
+let setIntervalId;
+
+let score = 0;
+
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreELement.innerHTML= `Puntuacion maxima: ${highScore}`;
+
+
+const handleGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("F");
+    location.reload();
+}
 
 //hacemos aleatoria las coordenadas de la comida
 const changFoodPosition = () =>{
@@ -23,16 +42,16 @@ const changFoodPosition = () =>{
 const changeDirection = (e) =>{
     
 
-    if(e.key === "ArrowUp" || e.key === "w") {
+    if ((e.key === "ArrowUp" || e.key === "w") && velocityY !== 1) {
         velocityX = 0;
         velocityY = -1;
-    }else if(e.key === "ArrowDown" || e.key === "s"){
+    } else if ((e.key === "ArrowDown" || e.key === "s") && velocityY !== -1) {
         velocityX = 0;
         velocityY = 1;
-    }else if(e.key === "ArrowLeft" || e.key === "a"){
+    } else if ((e.key === "ArrowLeft" || e.key === "a") && velocityX !== 1) {
         velocityX = -1;
         velocityY = 0;
-    }else if(e.key === "ArrowRight" || e.key === "d"){
+    } else if ((e.key === "ArrowRight" || e.key === "d") && velocityX !== -1) {
         velocityX = 1;
         velocityY = 0;
     }
@@ -43,15 +62,20 @@ const changeDirection = (e) =>{
 
 //generamos la comida y la cabeza
 const initgame = () => {
+    if(gameOver) return handleGameOver();
     let htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"></div>`;
 
     snakeX += velocityX;
     snakeY += velocityY;
 
-
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
+        //console.log("game over")
+        gameOver = true;
+    }
     
 
-    snakeBody[0] = [snakeX, snakeY]
+    
+    
 
     // Verificar si la cabeza de la serpiente alcanza la comida
     if (snakeX === foodX && snakeY === foodY) {
@@ -59,14 +83,33 @@ const initgame = () => {
         changFoodPosition();
         snakeBody.push([foodX, foodY])
        // console.log(snakeBody)
-    }
+       score++;
 
+       highScore = score >= highScore ? score : highScore;
+       localStorage.setItem("high-score", highScore);
+       scoreELement.innerHTML=`Puntuacion: ${score}`;
+
+       highScoreELement.innerHTML= `Puntuacion maxima: ${highScore}`;
+
+       
+       
+
+       
+    }
+    
+    
+    //crecimiento del cuerpo
     for(let i = snakeBody.length - 1; i>0; i--){
         snakeBody[i] = snakeBody[i-1];
     }
 
+    snakeBody[0] = [snakeX, snakeY];
+
     for(let i = 0; i < snakeBody.length; i++){
         htmlMarkup += `<div class="head" style="grid-area:${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
+            gameOver = true;
+        }
     }
 
     
@@ -80,7 +123,7 @@ changFoodPosition ()
 
 //para que la cabeza se mueva sola
 
-setInterval(initgame, 125)
+setIntervalId = setInterval(initgame, 125)
 
 //generamos la direccion de la teclas
 document.addEventListener("keydown", changeDirection);
